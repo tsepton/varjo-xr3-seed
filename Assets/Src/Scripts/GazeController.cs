630de9b98f33852536c8541e57b9e78df315ff9d
Thibaut Septon <thibaut.septon@unamur.be>
2023-03-08 12:49:09 +0100

    Add code for gameobject gaze collision

--- Assets/Src/Scripts/GazeController.cs
+++ Assets/Src/Scripts/GazeController.cs
@@ -1,13 +1,28 @@
+using System;
 using UnityEngine;
 using Varjo.XR;
+using Random = UnityEngine.Random;
 
-public class GazeController : MonoBehaviour
-{
-    private void Update()
-    {
+public class GazeController : MonoBehaviour {
+    public Camera headset;
+
+    private void Update() {
         var data = VarjoEyeTracking.GetGaze();
-        Debug.Log(data.gaze.origin);
-        Debug.Log(data.gaze.forward * 100);
-        Debug.DrawRay(data.gaze.origin, data.gaze.forward * 100,  Color.green);
+        var worldPerspective = headset.transform.TransformPoint(data.gaze.origin);
+        var worldPerspectiveForward = headset.transform.TransformDirection(data.gaze.forward);
+        if (Physics.Raycast(worldPerspective, worldPerspectiveForward, out RaycastHit hit, Mathf.Infinity)) {
+            // GameObject being looked at - do something
+            UpdateGoColor(hit.collider.gameObject);
+        }
+    }
+
+    private void UpdateGoColor(GameObject go) {
+        try {
+            go.GetComponent<Renderer>().material.color = Random.ColorHSV();
+        }
+        catch (Exception e) {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 }
